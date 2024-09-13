654137a0516afb708888fb7c630222f4393d7fcc
oopswhy <liuyangbjtu@163.com>
2024-09-13 16:00:16 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.oopswhy.net": dockerHub,
+  "quay.oopswhy.net": "https://quay.io",
+  "gcr.oopswhy.net": "https://gcr.io",
+  "k8s-gcr.oopswhy.net": "https://k8s.gcr.io",
+  "k8s.oopswhy.net": "https://registry.k8s.io",
+  "ghcr.oopswhy.net": "https://ghcr.io",
+  "cloudsmith.oopswhy.net": "https://docker.cloudsmith.io",
+  "ecr.oopswhy.net": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.oopswhy.net": dockerHub,
 };
 
 function routeByHosts(host) {
